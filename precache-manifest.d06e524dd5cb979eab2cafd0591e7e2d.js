0bd8118abe93e5592b15ebde543c0a5920f03b68
pulkit <pulkit1298@gmail.com>
2020-08-15 23:33:17 +0530

    Updates

---
+++ precache-manifest.d06e524dd5cb979eab2cafd0591e7e2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9871e8f790e7e85a85c71a9cdc27d2a6",
+    "url": "/sosio-assignment/index.html"
+  },
+  {
+    "revision": "842345e15fbc57d2ebb8",
+    "url": "/sosio-assignment/static/css/main.ae60ca8a.chunk.css"
+  },
+  {
+    "revision": "27f32d1b074985162e6f",
+    "url": "/sosio-assignment/static/js/2.48eb1bc2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sosio-assignment/static/js/2.48eb1bc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "842345e15fbc57d2ebb8",
+    "url": "/sosio-assignment/static/js/main.be1fd20b.chunk.js"
+  },
+  {
+    "revision": "798af12d7461bbe4282b",
+    "url": "/sosio-assignment/static/js/runtime-main.e40e09f7.js"
+  }
+]);
